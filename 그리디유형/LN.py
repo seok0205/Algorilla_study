367a193adebef6be3df8507db5c76177bab230ce
DONGGYU <rlaehdrb1993@naver.com>
2025-02-04 21:37:32 +0900

    0204_그리디

---
+++ 그리디유형/LN.py
@@ -0,0 +1,27 @@
+# N, M, K를 공백으로 구분해서 입력받기
+n, m, k = map(int, input().split())
+
+# N개의 수를 공백으로 구분하여 리스트로 받기
+data = list(map(int, input().split()))
+
+data.sort(reverse=True) # 받은 데이터를 정렬(원본 data변경)
+first = data[0]
+second = data[1]
+
+result = 0
+
+while m > 0:
+    for i in range(k):   # 가장 큰 수를 k 번 더한다
+        if m == 0:       # m 이 0 이라면 반복문 탈출
+            break
+        result += first
+        m -= 1
+    result += second      # 두번째로 큰 수를 한 번 더한다
+    m -= 1
+
+print(result)
+
+
+# 입력값
+5 8 3
+2 4 5 6 3
---
+++ 그리디유형/LN2.py
@@ -0,0 +1,22 @@
+# Large Number 다른 풀이법
+n, m, k = map(int, input().split())
+list = list(map(int, input().split()))
+
+list.sort(reverse=True)
+first = list[0]
+second = list[1]
+
+count = 0   # first의 값이 몇번 들어갈지 체크
+count += m//(k+1) * k  
+count += m%(k+1)
+
+result = 0
+
+result += count * first
+result += (m-count) * second
+
+print(result)
+
+# 입력값
+# 5 8 3
+# 2 4 5 6 3
