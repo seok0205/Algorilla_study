516d97355851e8eae1d78b060b584f05cfe1f120
jungseokyu <9526yu@naver.com>
2025-02-04 16:53:57 +0900

    0204_1065. 한수

--- 25.02/0203_1065.py
+++ 25.02/0203_1065.py
@@ -4,5 +4,15 @@
 N이 주어질 때, 1보다 크거나 같고, N보다 작거나 같은 한수의 개수를 출력하라.
 '''
 
-N = int(input())
+N = input()
 
+if int(N) < 100:
+    result = int(N)
+else:    
+    result = 99
+    for i in range(100, int(N)+1):
+        num = list(str(i))
+        if int(num[0]) - int(num[1]) == int(num[1]) - int(num[2]):
+            result += 1
+
+print(result)
