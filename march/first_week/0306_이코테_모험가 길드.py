6f635a2db0a58ff55f61ee7977cbb20bde98f29c
yeonji <imyeonji520@gmail.com>
2025-03-06 17:40:20 +0900

    0306 - 이코테 1장 그리디 (모험가 길드 코드 수정)

--- march/first_week/0306_이코테_모험가 길드.py
+++ march/first_week/0306_이코테_모험가 길드.py
@@ -10,18 +10,18 @@
 1 3 4 3 3 3
 '''
 
+# 공포도가 제일 높은 사람을 기준으로 그룹을 결성하면 많은 그룹을 가지지 못하기 때문에
+# 공포도가 제일 낮은 순으로 그룹 안에 넣고 그룹 다 차면 보내는 방식으로 해야 함
 N = int(input())
 arr = list(map(int, input().split()))
 sorted_arr = sorted(arr)
-count = 0
-idx = N - 1
-while idx >= 0:
-    coward = sorted_arr[idx]
-    if coward > len(sorted_arr):
-        idx -= 1
-        continue
-    else:
-        for _ in range(coward):
-            idx -= coward
-        count += 1
-print(count)
+group_count = 0
+members_count = 0
+
+for coward in sorted_arr:
+    members_count += 1
+    if members_count >= coward:
+        group_count += 1
+        members_count = 0
+
+print(group_count)
